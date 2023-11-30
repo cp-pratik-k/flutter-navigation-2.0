2079c404d31103dc8bdba7b1a73bae19988f8837
Pratik-canopas <pratik.k@canopas.com>
2023-11-30 11:24:42 +0530

    Removed path strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'navigator.dart';
 
 void main() {
-  usePathUrlStrategy();
   runApp(const MyApp());
 }
 
